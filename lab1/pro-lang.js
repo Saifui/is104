2f0dfef1d00f6a09da363909735c4b1c6418ae2e
Saifui <Saiful_jamaris@live.no>
2013-10-23 13:09:01 +0200

    lab1

---
+++ lab1/pro-lang.js
@@ -0,0 +1,9 @@
+
+function printToOutputBox(selectobj){
+ var e_proglangs = document.getElementById("pro-lang");
+ var e_outputbox = document.getElementById("outputbox");
+ e_outputbox.innerHTML = "Du har selektert programmeringsspr&aring;ket " + e_proglangs.options[selectobj.selectedIndex].text;
+}
+
+
+
